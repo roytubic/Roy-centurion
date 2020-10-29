5f254ef61fa6ce051fca2886b8ba0a070f2ae8eb
Roy-Tubic <roytubic@gmail.com>
2020-10-29 15:53:00 +1300

    Initial function set up and working device view change

---
+++ app.js
@@ -0,0 +1,25 @@
+document.getElementById("timer").addEventListener("click", function(){
+    let timeLeft = 60 * 100;
+    display = document.querySelector('#timer');
+    startTimer(timeLeft, display);
+
+    function startTimer(duration, display) {
+        let timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent = minutes + ":" + seconds;
+    
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
+    }
+});
+
+
+
